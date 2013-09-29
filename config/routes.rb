6aa42e7083d5935167eb6c78ca18478c10aedef6
Amy Ton <amyhton@gmail.com>
2013-09-28 20:49:55 -0500

    Change the route to /adventures

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Railsgirls::Application.routes.draw do
   resources :adventures
 
+  root :to => redirect('/adventures')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
